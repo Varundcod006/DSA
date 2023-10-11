b7eece427da40d34ed1efd9ba23330a4e6624c9a
Rajendrashinh Jadeja <114151556+Varundcod006@users.noreply.github.com>
2023-10-11 07:11:38 +0000

    Memory Allocation L1

---
+++ Dynamic_Mem/Main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+// Mamory Allocation In C
+
+void Mlc(){
+    int a [5];
+    int *ptr;
+
+    // Do Somting With The Pointer
+}
+
+int main(){
+    printf("C Is Ready");
+    return 0;
+}
