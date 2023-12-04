e16b2583261c04588ece14c53b7d36f83d0dad65
Rajendrashinh Jadeja <114151556+Varundcod006@users.noreply.github.com>
2023-12-04 10:10:19 +0000

    Unit 5 Started With Introducton

---
+++ Non-Linear-Data-Structure/Trees/main.js
@@ -0,0 +1,16 @@
+tree = {
+    var1: "Hello",
+    var2: "India",
+    info: {
+        "list": {
+            j: 10
+        },
+        "Just": {
+            k: 20
+        }
+    },
+    info2: {
+        "String": "Type c"
+    }
+}
+console.log(tree.var2)
---
+++ Non-Linear-Data-Structure/Trees/main.py
@@ -0,0 +1,14 @@
+tree = {
+    "var1": "Hello",
+    "var2": "India",
+    "info": {
+        "list": {
+            "j": 10
+        },
+        "Just": {
+            "k": 20
+        }
+    }
+}
+
+print(tree["var1"])
